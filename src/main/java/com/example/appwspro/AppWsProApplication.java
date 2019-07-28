987e96c1c40e103c3e4d0a007da70855441069a0
Valery Emelyanov <emelva@rambler.ru>
2019-07-29 00:46:16 +0400

    Initialization. Simple REST implementation

---
+++ src/main/java/com/example/appwspro/AppWsProApplication.java
@@ -0,0 +1,13 @@
+package com.example.appwspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWsProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWsProApplication.class, args);
+	}
+
+}
